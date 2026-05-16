5f0300677e1cf00787b2f940887ef1157b17d4e0
AlonsoOspino <20232105@aloe.ulima.edu.pe>
2026-05-15 19:44:10 -0500

    jioafojafoi

--- migration-uidesign/backend/app.js
+++ migration-uidesign/backend/app.js
@@ -21,7 +21,8 @@ const cors = require("cors");
 const app = express();
 const PORT = Number(process.env.PORT || 3000);
 app.use(cors()); 
-app.use(express.json());
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ extended: true, limit: "25mb" }));
 
 app.get("/health", async (_req, res) => {
   try {
